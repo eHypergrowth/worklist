06f791260afa25abb0c4c3bd4837d76fe82bb349
alan1606 <mauri.ar3@gmail.com>
2021-07-15 19:55:38 -0600

    Mensaje creado por variables

--- src/main/java/com/mycompany/hapi/prueba.java
+++ src/main/java/com/mycompany/hapi/prueba.java
@@ -3,12 +3,14 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package com.mycompany.hapi;
 
 /**
  *
  * @author alanm
  */
+
 import ca.uhn.hl7v2.DefaultHapiContext;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.HapiContext;
@@ -37,13 +39,47 @@ public class prueba {
         HapiContext context = new DefaultHapiContext();
         context.getParserConfiguration().setValidating(false);
 
-        String msg = "MSH|^~\\&|BROKER|TINTEGRO|DCM4CHEE|TINTEGRO|||OMI^O23|12345|P|2.5.1\r"
-                + "PID|||C8F26D5||PACIENTE N DE^PRUEBA||1990505|M\r"
-                + "PV1||||||||1234^CMP123|||||||A0||||908831\r"
-                + "ORC|NW|12359|12359||SC\r"
-                + "TQ1|||||||202107081930||R\r"
-                + "OBR||||||||||||||||1234^CMP123||||||||||||||||||||||||||||ABDOMEN TOTAL SIMPLE\r"
-                + "IPC|12359|CODPROCEDIMIENTO|12359|CODPROCEDIMIENTO|CT||CT111185||111185\r";
+        //MSH
+        String aplicacionEnvia = "MWLJAVA"; //MAX 227
+        String sendingFacility = "API"; //MAX 227
+        String receivingApplication = "DCM4CHEE"; //MAX 227
+        String receivingFacility = sendingFacility; //SAME
+        String idMessageControl = "123456"; //MAX 20
+        
+        //PID
+        String patientId = "AURA000616HCHGBLA6"; //MAX 250 va a ser la curp
+        String nombrePaciente = "PACIENTE^FAKE"; //MAX 250
+        String birthday = "20000616"; //MAX 26
+        String sex = "F"; //MALE, FEMALE, AMBIGUOUS, NOT APPLICABLE, OTHER, UKNOW
+        
+        //ORC
+        String control = "NW"; //NW = NUEVO, 2 DÍGITOS, 
+        String iDNIfOptional = "11112"; //MAX 22
+        
+        //TQ1
+        String dateTime = "202107151930"; //26 HORA INICIO
+        String priority = "R"; //De rutina MAX 250
+        
+        //OBR
+        String proveedor = "1234^CMP123"; //Creo que es opcional MAX 250 //Es el medico que refiere
+        String procedure = "UN CR DE PRUEBA"; //PROCEDIMIENTO MAX 250
+        
+        //IPC
+        String accessionNumber = "5"; //MAX 80
+        String solProcedure = "CR PRUEB"; //MAX 22 //este es el nombre como aparece en el pacs requested
+        String uidIdStudyInstance = "C8F4209"; //MAX 70
+        String idStepProcedure = "CR PROC"; //MAX 22
+        String modality = "CR"; //MAX 16
+        String stationName = "FCR-CSL"; //MAX 22
+        String scheduledAE = "FCR-CSL" ; //MAX 16
+        
+        String msg = "MSH|^~\\&|" + aplicacionEnvia + "|" + sendingFacility + "|" + receivingApplication+"|" + receivingFacility+ "|||OMI^O23|" + idMessageControl+ "|P|2.5.1\r"
+                + "PID|||"+ patientId +"||" + nombrePaciente + "||" + birthday + "|" + sex+ "\r"
+                //+ "PV1||||||2359||1234^CMP123|||||||A0||||908831\r" 
+                + "ORC|" + control + "|" + iDNIfOptional+ "|" +iDNIfOptional + "||SC\r"
+                + "TQ1|||||||" + dateTime + "||" + priority +"\r"
+                + "OBR||||||||||||||||" + proveedor + "||||||||||||||||||||||||||||" + procedure +"\r"
+                + "IPC|" + accessionNumber + "|" + solProcedure + "|" + uidIdStudyInstance + "|" + idStepProcedure + "|" + modality +"||" + stationName +"||" + scheduledAE +"\r";
 
         Parser p = context.getPipeParser();
         Message adt = null;
@@ -63,16 +99,10 @@ public class prueba {
             String responseString = p.encode(response);
             System.out.println("Received response:\n" + responseString);
 
-        } catch (HL7Exception ex) {
+        } catch (HL7Exception | LLPException | IOException  ex  ) {
             Logger.getLogger(prueba.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (LLPException ex) {
-            Logger.getLogger(prueba.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (IOException ex) {
-            Logger.getLogger(prueba.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
+        }finally {
             connection.close();
         }
-
     }
-
 }
